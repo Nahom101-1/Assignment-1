20704d33c4420bf812d4d66a45012c064688e794
Nahom Berhane <nahomberhane10@gmail.com>
2026-09-16 14:42:22 +0200

    added query class for client

--- src/main/java/com/ass1/client/Client.java
+++ src/main/java/com/ass1/client/Client.java
@@ -1,5 +1,23 @@
 package com.ass1.client;
+import java.util.ArrayList;
+
+/**
+ * @param method name of the method
+ * @param args list of arguments
+ * @param zone requested zone
+ */
+class Query {
+    String method;
+    String[] args;
+    int zone;
+
+    Query(String method, String[] args, int zone){
+        this.method = method;
+        this.args = args;
+        this.zone = zone;
+    };
+}
 
 public class Client {
-    
+    public ArrayList<Query> queries = new ArrayList<>();
 }
