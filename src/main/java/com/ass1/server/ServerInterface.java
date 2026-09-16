59695f8eb41d1b385695e330e4c50691fa489d60
Alexander Gran Makarov <143446995+v0rakam@users.noreply.github.com>
2026-09-16 14:13:53 +0200

    Update ServerInterface.java

--- src/main/java/com/ass1/server/ServerInterface.java
+++ src/main/java/com/ass1/server/ServerInterface.java
@@ -3,9 +3,9 @@ import java.rmi.*;
 
 interface ServerInterFace extends Remote {
 
-    QueryResult getPopulationofCountry(String countryName); 
-    QueryResult getNumberofCities(String countryName, int threshold, String comp); 
-    QueryResult getNumberofCountries(String citycount, int threshold, String comp); 
-    QueryResult getNumberofCountriesMM(int citycount, int minpopulation, int taxpopulation); 
+    QueryResult getPopulationofCountry(String countryName, int clientZone); 
+    QueryResult getNumberofCities(String countryName, int threshold, String comp, int clientZone); 
+    QueryResult getNumberofCountries(String citycount, int threshold, String comp, int clientZone); 
+    QueryResult getNumberofCountriesMM(int citycount, int minpopulation, int taxpopulation, int clientZone); 
     int getCurrentWorkload(); 
 }
